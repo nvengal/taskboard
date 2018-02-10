38d8be75261f4edd3fa72acb826c21e73612c608
vengal01 <vengal01@email.franklin.edu>
2018-02-09 22:58:12 -0500

    reorganized classes

--- src/main/java/taskboard/Application.java
+++ src/main/java/taskboard/Application.java
@@ -1,4 +1,4 @@
-package controllers;
+package taskboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/taskboard/controllers/MainController.java
+++ src/main/java/taskboard/controllers/MainController.java
@@ -1,4 +1,4 @@
-package controllers;
+package taskboard.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class BaseController {
+public class MainController {
 
     @RequestMapping("/")
     public String index(@RequestParam(value = "name",
