8c301c3816d29f15ff3ab98d65729305d05dc9db
U-HEADQUARTERS\nvengal <nvengal@nvengal-win0.headquarters.healthedge.com>
2018-03-11 13:29:08 -0400

    fixes bug with duplicate user registrations

--- src/main/resources/static/javascript/registration.js
+++ src/main/resources/static/javascript/registration.js
@@ -28,22 +28,26 @@ $( () => {
         url:'/api/users/add',
         data: JSON.stringify(user),
         success: (response) => {
-          $.ajax({
-                type: 'POST',
-                datatype: 'json',
-                headers: {
-                  'Accept': 'application/json',
-                  'Content-Type': 'application/json'
-                },
-                url:'/api/users/verify',
-                data: JSON.stringify(user),
-                success: (response) => {
-                  window.location.href = window.location.origin + '/home';
-                },
-                error: () => {
-                  alert('Verification error');
-                }
-              });
+          if (response.success) {
+            $.ajax({
+              type: 'POST',
+              datatype: 'json',
+              headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+              },
+              url:'/api/users/verify',
+              data: JSON.stringify(user),
+              success: (response) => {
+                window.location.href = window.location.origin + '/home';
+              },
+              error: () => {
+                alert('Verification error');
+              }
+            });
+          } else {
+            alert(response.message);
+          }
         },
         error: (err) => {
           alert('Failed to save new user');
