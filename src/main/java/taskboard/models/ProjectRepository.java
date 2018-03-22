464e1666c3933be6db61a7b5e29193afa71bc59e
groves13 <groves13@email.franklin.edu>
2018-03-22 08:31:45 -0400

    Changed Project retrieval

--- src/main/java/taskboard/models/ProjectRepository.java
+++ src/main/java/taskboard/models/ProjectRepository.java
@@ -1,7 +1,14 @@
 package taskboard.models;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ProjectRepository extends CrudRepository<Project, Long> {
+
+    @Query("SELECT p FROM Project t WHERE t.project = :project")
+    List<Task> findTasks(@Param("project")Project project);
 
 }
